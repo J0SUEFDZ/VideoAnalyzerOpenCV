8eb6828e9e500ed8e9f1695922aa616a2c741438
J0SUEFDZ <josue.0795@gmail.com>
2019-05-07 20:53:49 -0600

    Tracking using ROI

--- analyzer.py
+++ analyzer.py
@@ -2,10 +2,24 @@ import cv2
 import numpy as np
 
 
+class Detector():
+    def __init__(self, fileInput):
+        self.videoPath = fileInput
+
+
 class Tracker:
-    def __init__(self, fileInput, ):
+    def __init__(self, fileInput):
         self.videoPath = fileInput
         self.tracker = None
+        self.heigth = 0
+        self.width = 0
+
+    def resize(self, frame):
+        size_frame = 2
+        new_heigth = int(self.heigth/size_frame)
+        new_width = int(self.width/size_frame)
+        resized_frame = cv2.resize(frame, (new_width, new_heigth))
+        return resized_frame
 
     def getTrackerByName(self, tracker_name='KCF'):
         tracker_name = tracker_name.upper()
@@ -22,22 +36,58 @@ class Tracker:
         try:
             self.tracker = trackers[tracker_name]
         except KeyError:
-            print('Wrong Tracker name')
+            raise KeyError('Wrong Tracker name')
         except Exception:
             raise
 
-    def trackMario(self):
+    def tracking(self):
         cap = cv2.VideoCapture(self.videoPath)
+
+        # Resize image since is too big
+        self.heigth = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        self.width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.getTrackerByName()
+
+        success, frame = cap.read()
+        frame = self.resize(frame)
+
+        if not success:
+            raise ValueError('File not found.')
+
+        # Select bounding box with ROI
+        bbox = cv2.selectROI(frame, False)
+        success = self.tracker.init(frame, bbox)
 
         while cap.isOpened():
             success, frame = cap.read()
+            frame = self.resize(frame)
             if not success:
                 break
-            cv2.imshow("Mario", frame)
+
+            success, bbox = self.tracker.update(frame)
+            if success:
+                point1 = (int(bbox[0]), int(bbox[1]))
+                point2 = (int(bbox[0]+bbox[2]), int(bbox[1]+bbox[3]))
+                cv2.rectangle(frame, point1, point2, (255, 0, 0), 2, 1)
+            else:
+                cv2.putText(frame, "Failed.")
+
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord('q'):  # Q Key pressed
+                break
+            elif key == ord('s'):  # S Key pressed
+                time.sleep(5)
+            elif key == ord('t'):  # T Key pressed
+                input('Any key to continue')
+
+            cv2.imshow("Ball Rolling", frame)
+
+        cv2.release()  # Dont forget to release your video!
+        cv2.destroyAllWindows()  # Close all the opened windows
 
 if __name__ == "__main__":
-    videoPath = 'assets/video.mp4'
+    videoPath = 'assets/vid2.mp4'
 
     tracker = Tracker(videoPath)
-    tracker.getTrackerByName()
-    tracker.trackMario()
+    tracker.tracking()
