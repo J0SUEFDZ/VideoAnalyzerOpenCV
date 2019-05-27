6b3539a79f5ded253889ef8abdc786a5eee06f22
J0SUEFDZ <josue.0795@gmail.com>
2019-05-26 18:25:51 -0600

    Finished version.

--- analyzer.py
+++ analyzer.py
@@ -5,7 +5,6 @@ from time import sleep
 
 class Detector:
     """
-    Test for detect moving on OpenCV:
     Source Video: https://www.youtube.com/watch?v=heds_qDFqsA
     """
     def __init__(self, fileInput):
@@ -17,21 +16,17 @@ class Detector:
         params = cv2.SimpleBlobDetector_Params()
 
         params.filterByColor = True
-        params.blobColor = 255  # 0 is white, 255 is black.
+        params.blobColor = 255
 
         return params
 
-    def video_writer(self):
-        files = ["blur", "color", "diff", "keypoints"]
-        videos = []
-        for file_to_write in files:
-            out = cv2.VideoWriter(
-                file_to_write+'mp4',
-                cv2.VideoWriter_fourcc('M','J','P','G'),
-                10, (self.width, self.height)
-            )
-            videos.append(out)
-        return videos
+    def video_writer(self, file_to_write):
+        out = cv2.VideoWriter(
+            "assets/"+file_to_write+'.avi',
+            cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+            30, (int(self.width), int(self.height))
+        )
+        return out
 
     def detect_movement(self):
         cap = cv2.VideoCapture(self.videoPath)
@@ -43,17 +38,15 @@ class Detector:
         _, first_frame = cap.read()
 
         detector = cv2.SimpleBlobDetector_create(self.getBlobParams())
+        red_video = self.video_writer('red')
+        diff_video = self.video_writer('diff')
+        keypoints_video = self.video_writer('keypoints')
         while cap.isOpened():
             success, frame = cap.read()
             if not success:
                 break
-            # 1 - Applying blur and thresold.
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-            thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-            cv2.imshow("1- Blur and Threshold", thresh)
 
-            # 2 - Applying a mask per color.
+            # 1 - Applying a mask per color.
             lower_red = np.array([0, 130, 75])
             upper_red = np.array([255, 255, 255])
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -62,20 +55,20 @@ class Detector:
                 mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
             )
             red = cv2.bitwise_and(frame, frame, mask=mask)
-            cv2.imshow("2- Only Red", red)
+            cv2.imshow("1- Only Red", red)
+            red_video.write(red)
 
-            # 3- Difference between first frame and the rest.
+            # 2- Difference between first frame and the rest.
             diff = cv2.absdiff(first_frame, frame)
-            cv2.imshow("3- Difference", diff)
+            cv2.imshow("2- Difference", diff)
+            diff_video.write(diff)
 
-            # 4- Gey coordinates of the ball.
+            # 3- Gey coordinates of the ball.
             keypoints = detector.detect(frame)
             im_with_keypoints = frame.copy()
             if keypoints:
                 x_pt, y_pt = int(keypoints[0].pt[0]), int(keypoints[0].pt[1])
                 ball_size = round(keypoints[0].size/2)
-                # Size is actually the diameter
-                # what we need is radius, that why we divide by 2.
                 cv2.circle(
                     im_with_keypoints,  # Image output
                     (x_pt, y_pt),  # X, Y coordiantes
@@ -83,7 +76,8 @@ class Detector:
                     (255, 0, 0),  # Color, BGR.
                     3  # Thickness of the line.
                 )
-            cv2.imshow('Keypoints', im_with_keypoints)
+            cv2.imshow("3- Keypoints", im_with_keypoints)
+            keypoints_video.write(im_with_keypoints)
 
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):  # Q Key pressed, quit everything.
